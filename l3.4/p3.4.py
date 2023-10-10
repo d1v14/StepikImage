42f6a226c515abdf60a3abfd1a6d92c5780116c9
Novikov Dmitry <dmitriinov12@gmail.com>
2023-10-10 14:18:55 +0300

    changed plt.hist to np.histogram and corrected the function

--- l3.4/p3.4.py
+++ l3.4/p3.4.py
@@ -2,9 +2,9 @@ from skimage.io import imread,imshow,imsave
 from matplotlib import pyplot as plt
 import numpy as np
 img = imread("l3.4/img.png")
-a,b,c = plt.hist(img.ravel(),bins = range(257))
 img_o = np.array(img)
 mi = 0
+a,_= np.histogram(img.ravel(),bins=range(257))
 for i in range(len(a)):
     if(mi!=0):
         break
@@ -12,6 +12,6 @@ for i in range(len(a)):
         mi+=a[i]
 for i in range(len(img)):
     for j in range(len(img[0])):
-        img_o[i,j] = np.round((a[:img[i,j]].sum()-mi)/(img.shape[0]*img.shape[1]-1)*255)
+        img_o[i,j] = np.round((a[:img[i,j]+1].sum()-mi)/(img.shape[0]*img.shape[1]-1)*255)
 imshow(img_o)
 plt.show()
